db27016bdd33253f0025f97883a714b3a88424f7
Rena Goldsmith <rena.kg@gmail.com>
2016-07-11 11:23:09 +1200

    render buttons for counter

--- index.js
+++ index.js
@@ -17,16 +17,43 @@ const counter = (state = 0, action) => {
 
 const store = createStore(counter) // points to the counter fn on line 3
 
+const Counter = ({
+    value,
+    onIncrement,
+    onDecrement
+}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+)
 
-const show = () => {
-    render(<Counter value={store.getState()}/>, document.getElementById('root'))
+const renderer = () => {
+    render (
+        <Counter
+            value={store.getState()}
+            onIncrement={ () =>
+                store.dispatch({
+                    type: 'INCREMENT'
+                })
+            }
+            onDecrement={ () =>
+                store.dispatch({
+                    type: 'DECREMENT'
+                })
+            }
+        />
+        , document.getElementById('root')
+    )
 }
 
-store.subscribe(show)
+store.subscribe(renderer)
+renderer()
 
-document.addEventListener('click', () => {
-    store.dispatch({ type: 'INCREMENT' })
-})
+// document.addEventListener('click', () => {
+//     store.dispatch({ type: 'INCREMENT' })
+// })
 
 // console.log(store.getState()) //grabbed the initial state = 0 declared in parameter
 
