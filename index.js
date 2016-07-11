f5bf4d017fff744eccb9f38b8d0ef94131ae6bb2
Troy Lamerton <troy.lamerton@gmail.com>
2016-07-11 14:50:07 +1200

    Add plus and minus buttons, coutner working

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 import { createStore } from 'redux'
 import React from 'react'
-import { render } from 'react-dom'
+import { render as renderDom } from 'react-dom'
 
 const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            return state
-    }
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state
+  }
 }
 
-
-
-const store = createStore(counter) // points to the counter fn on line 3
-
-
-const show = () => {
-    render(<Counter value={store.getState()}/>, document.getElementById('root'))
+const Counter = ({value, onPlus, onMinus}) => {
+  return (
+    <div style={{fontSize: "100px", textAlign: 'center'}}>
+      {value}
+      <br/>
+      <button onClick={onPlus} ><pre> + </pre></button>
+      <button onClick={onMinus} ><pre> - </pre></button>
+    </div>
+  )
 }
 
-store.subscribe(show)
+const store = createStore(counter, window.devToolsExtension && window.devToolsExtension()) // points to the counter fn on line 3
 
-document.addEventListener('click', () => {
-    store.dispatch({ type: 'INCREMENT' })
-})
+const updatePage = () => {
+  renderDom (
+    <Counter 
+      value={store.getState()} 
+      onPlus={() => {store.dispatch({ type: 'INCREMENT' })}} 
+      onMinus={() => {store.dispatch({ type: 'DECREMENT' })}}
+    />, document.getElementById('root'))
+}
+
+store.subscribe(updatePage) // run this function every time the state changes
+updatePage()
 
 // console.log(store.getState()) //grabbed the initial state = 0 declared in parameter
 
