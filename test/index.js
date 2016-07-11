957d0688789761db402428f7e2f839f027ed915b
Rena Goldsmith <rena.kg@gmail.com>
2016-07-11 12:00:36 +1200

    set up test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,28 @@
 var test = require('tape')
+var expect = require('expect')
+var deepFreeze = require('deep-freeze')
 
-test('testing counter', function (t) {
-  t.ok(true)
-  t.end()
-})
+const toggleTodo = (todo) => {
+    return 1
+}
+
+const testToggleTodo = () => {
+    const todoBefore = {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+    }
+    const todoAfter = {
+        id: 1,
+        text: 'Learn Redux',
+        completed: true
+    }
+
+    expect(
+        toggleTodo(todoBefore)
+    ).toEqual(todoAfter)
+}
+
+
+testToggleTodo()
+console.log('All tests pass')
